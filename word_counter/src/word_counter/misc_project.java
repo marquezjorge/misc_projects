aca798fb34899c34c0340ef7c8fae964268b87b5
Jorge Marquez <marquezjorge54@gmail.com>
2020-01-29 18:30:34 -0800

    initial commit

--- word_counter/src/word_counter/misc_project.java
+++ word_counter/src/word_counter/misc_project.java
@@ -1,15 +1,14 @@
 package word_counter;
 
-import javax.swing.*;
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class misc_project 
 {
 	public static void main(String[] args)
 	{
-		WordCount test = null;
-		
-		test.getString();
+		WordCount test = new WordCount();
+		test.getUserString();
 		System.out.print(test.count());
 		
 	}
@@ -20,25 +19,39 @@ class WordCount
 	private static String userInput = ""; 
 	static Scanner input = new Scanner(System.in);
 	
-	public void getString() 
+	public WordCount()
 	{
-		System.out.print("Enter a string or paragrahp of text to get word count");
-		userInput = input.nextLine();
+		userInput = "";
+	}
+	
+	public void getUserString() 
+	{
+		System.out.println("Enter a string or paragrahp of text to get word count");
+		System.out.print("-> ");
+		setString(input.nextLine());
+		//userInput = input.nextLine();
 	}
 	
 	public int count()
 	{
 		int count = 0;
+		Pattern p = Pattern.compile("a-zA-Z");
 		
 		for(int i = 0; i < userInput.length() + 1; i++)
 		{
-			count++;
+			Matcher m = p.matcher(getString());
 		}
 		
 		return count;
 	}
 	
+	private void setString(String inputIn) 
+	{
+		userInput = inputIn;
+	}
 	
+	private String getString()
+	{
+		return userInput;
+	}
 }
-
-// figure out how to read strings with O(n) complexity
